d4184ab2e7939bd0278099af69907cd66338da51
Rodolphe <rodolphebansard@yahoo.com>
2022-04-27 13:16:30 +0200

    display resume

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {RouteService} from "../../services/route.service";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
+  constructor(private routeService: RouteService) { }
 
-  ngOnInit(): void {
+  goToContact(){
+    this.routeService.goToContact();
   }
 
 }
