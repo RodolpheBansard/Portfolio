e0de9ee835f9496b73fd9e01672c2b2653738f68
Rodolphe <rodolphebansard@yahoo.com>
2022-04-29 16:13:44 +0200

    list images

--- src/app/components/work/work-card/work-card.component.ts
+++ src/app/components/work/work-card/work-card.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {TiltSettings2} from "../../home/tiltSettings";
 import {Work} from "../work.component";
 import {WorkModalService} from "../../../services/work-modal.service";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-work-card',
@@ -17,10 +19,20 @@ export class WorkCardComponent implements OnInit {
 
   tiltSetting = TiltSettings2;
 
-  constructor( private workModalService:WorkModalService) { }
+  previewImageUrl$! : Observable<string>;
+
+  constructor( private workModalService:WorkModalService,
+               private storage: AngularFireStorage) {
+
+
+  }
 
   ngOnInit(): void {
+    console.log(this.work.previewPath)
+    this.previewImageUrl$ = this.storage.ref('works/'+ this.work.title +'/1.png').getDownloadURL();
   }
+
+
 
   onClickWork(work:Work){
     this.workModalService.updateSelectedWork(work);
--- src/app/components/work/work-details-modal/work-details-modal.component.ts
+++ src/app/components/work/work-details-modal/work-details-modal.component.ts
@@ -17,7 +17,7 @@ export class WorkDetailsModalComponent implements AfterViewInit {
 
   counter = 1;
 
-  imageUrl!: Observable<string>;
+  imageUrl: string[]= [];
 
   constructor(private storage: AngularFireStorage,
               private workModalService:WorkModalService) {
@@ -26,37 +26,38 @@ export class WorkDetailsModalComponent implements AfterViewInit {
 
   nextImage(){
     // @ts-ignore
-    if(this.counter >= this.work.tags.length){
-      this.counter = 1;
+    if(this.counter >= this.work.tags.length-1){
+      this.counter = 0;
     }
     else{
       this.counter++;
     }
-    this.changeImage();
   }
   previousImage(){
-    if(this.counter <= 1){
+    if(this.counter <= 0){
       // @ts-ignore
-      this.counter = this.work.tags.length;
+      this.counter = this.work.tags.length-1;
     }
     else{
       this.counter--;
     }
-    this.changeImage();
   }
 
-  changeImage(){
-    if(this.work){
-      console.log(this.work);
-      // @ts-ignore
-      this.imageUrl = this.storage.ref('works/'+ this.work.title +'/'+ this.counter +'.png').getDownloadURL();
-      console.log(this.imageUrl);
-    }
 
-  }
+
 
   ngAfterViewInit(): void {
-    this.changeImage();
+    if (this.work) {
+      // @ts-ignore
+      this.work.illustrationsPath.forEach((image, index) => {
+        // @ts-ignore
+        this.storage.ref('works/' + this.work.title + '/' + image + '.png').getDownloadURL().subscribe(
+          (url) => {
+            this.imageUrl[index] = url;
+          }
+        );
+      })
+    }
   }
 
   resetSelectedWork(){
